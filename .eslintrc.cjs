a16909c7947bb38a47e40943df4b8ac9701d13f1
Naimur <naeemhasan28@gmail.com>
2023-07-14 15:15:48 +0600

    initial setup done of vite react with eslint, prettier, husky, lint-staged, tailwind, typescript, react-router and daisy ui

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+  },
+  ignorePatterns: [
+    '*.config.js',
+    '*.config.ts',
+    '*.config.cjs',
+    '.eslintrc.cjs',
+  ],
+};
---
+++ src/routes/index.tsx
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Homepage from '../pages/Homepage';
+
+const router = createBrowserRouter([{ path: '/', element: <Homepage /> }]);
+
+export default router;
